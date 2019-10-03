9c35d7c35476de7b7a92e5fd84abaafd45eb8fef
J-B Garway-Templeman <jbgarwayt@gmail.com>
2019-10-03 09:40:37 +0100

    user and profile viewing distinction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,11 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    @profile = User.find(params[:id])
   end
 
   def edit_password
-    @user = User.find(params[:id])
+    @profile = User.find(params[:id])
   end
 
   # POST /users
@@ -52,26 +52,26 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    @profile = User.find(params[:id])
 
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @profile.update(user_params)
+        format.html { redirect_to @profile, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update_password
-    @user = User.find(params[:id])
+    @profile = User.find(params[:id])
   end
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @profile.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
@@ -81,7 +81,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @profile = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
